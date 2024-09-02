861844c7435fc77bd0cd361f2471a5ec0cf671fa
Pat Awayan <awayan.patricia@gmail.com>
2024-09-03 07:04:17 +0800

    Firebase fixing

--- src/components/Home/HomeFilters.vue
+++ src/components/Home/HomeFilters.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
+import { computed, ref } from 'vue'
 import InputCheckbox from '../common/input/InputCheckbox.vue'
 import InputDropdown from '../common/input/InputDropdown.vue'
 import { ForagingLocations, RoomBundleMap, RoomTypes, Seasons, SourceType } from '@/data/types'
